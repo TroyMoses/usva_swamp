9489fa884048dc59ca3171a862772fdade220c5a
TroyMoses <mugabimoses07@gmail.com>
2023-08-11 13:57:20 +0300

    subscribe page updated

--- app/subscribe/page.jsx
+++ app/subscribe/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
 import Validation from '../../utils/subscribeValidation.jsx';
 import axios from 'axios';
@@ -19,7 +19,18 @@ export default function SubscribePage() {
         category: ''
     });
     
-    const [errors, setErrors] = React.useState({})
+    const [errors, setErrors] = React.useState({});
+    const [buttonDisabled, setButonDisabled] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+
+    useEffect(() => {
+      if(values.fname.length > 0 && values.lname.length > 0 && values.gender.length > 0 && values.email.length > 0 && values.category.length > 0) {
+        setButonDisabled(false);
+      } else {
+        setButonDisabled(true);
+      }
+    }, [values]);
+    
 
     const handleSubmit = async(e) => {
 
@@ -28,6 +39,7 @@ export default function SubscribePage() {
 
         if(errors.fname === "" && errors.lname === "" && errors.gender === "" && errors.email === "" && errors.category === "") {
             try {
+                setLoading(true);
                 const res = await fetch('/api/member', {
                     method: 'POST',
                     headers: {
@@ -37,9 +49,12 @@ export default function SubscribePage() {
                 });
                 await res.json();
                 router.push('/');
+
             } catch (error) {
                 console.error(error.message);
-            } 
+            } finally {
+                setLoading(false);
+            }
         }
 
     };
@@ -81,8 +96,16 @@ export default function SubscribePage() {
                         onChange={(e) => setValues({...values, category:e.target.value})}/>
                         {errors.category && <span className='text-red-600'>{errors.category}</span>}
                     </div>
+                    <div>
+                        <h2 className='text-center font-sans text-gray-600'>{loading ? 'Registering, please wait....' : ''}</h2>
+                    </div>
 
-                    <button type='submit' className='w-full bg-brightRed text-white hover:bg-red-500 rounded-lg border-2 my-2 py-2'>Register</button>
+                    <div>
+                        <h1 className='text-center text-gray-800'>Note: Please register ONCE.</h1>
+                        <h2 className='text-center font-sans text-gray-600'>{loading ? '' : '(After registering, check your inbox, you will be notified through email. )'}</h2>
+                    </div>
+
+                    <button type='submit' className='w-full bg-brightRed text-white hover:bg-red-500 rounded-lg border-2 my-2 py-2'>{buttonDisabled ? 'Please Enter Details' : 'Register'}</button>
                 </form>
             </div>
         </div>
